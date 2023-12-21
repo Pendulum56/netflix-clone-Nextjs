0d554f34459e2b809e066191ca843e9af2fb03e3
tiny <1119663769@qq.com>
2023-12-22 02:10:48 +0800

    google登录无法使用，完成登录页

--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -2,14 +2,14 @@ import Input from '@/components/Input'
 import { useCallback, useState } from 'react'
 import { signIn } from 'next-auth/react'
 import axios from 'axios'
-import { useRouter } from 'next/router'
+import { FcGoogle } from 'react-icons/fc'
+import { FaGithub } from 'react-icons/fa'
 
 const Auth = () => {
   const [email, setEmail] = useState('')
   const [name, setName] = useState('')
   const [password, setPassword] = useState('')
   const [variant, setVariant] = useState('login')
-  const router = useRouter()
 
   // 切换登录注册显示
   const toggleVariant = useCallback(() => {
@@ -24,14 +24,12 @@ const Auth = () => {
       await signIn('credentials', {
         email,
         password,
-        redirect: false,
-        callbackUrl: '/',
+        callbackUrl: '/profiles',
       })
-      router.push('/')
     } catch (error) {
       console.log(error)
     }
-  }, [email, password, router])
+  }, [email, password])
 
   // 注册
   const register = useCallback(async () => {
@@ -88,6 +86,20 @@ const Auth = () => {
             >
               {variant === 'login' ? 'Login' : 'Sign up'}
             </button>
+            <div className=" flex flex-row items-center gap-4 mt-8 justify-center">
+              <div
+                onClick={() => alert('google无法使用😈github可以正常使用')}
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+              >
+                <FcGoogle size={30} />
+              </div>
+              <div
+                onClick={() => signIn('github', { callbackUrl: '/profiles' })}
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+              >
+                <FaGithub size={30} />
+              </div>
+            </div>
             <p className="text-neutral-500 mt-12">
               {variant === 'login'
                 ? 'First time using Netflix?'
