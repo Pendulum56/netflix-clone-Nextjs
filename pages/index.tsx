2e4febe6c53b3939f8b4957b33d1447a096e808b
tiny <1119663769@qq.com>
2023-12-22 04:52:04 +0800

    导航栏

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,28 @@
-import useCurrentUser from '@/hooks/useCurrentUser'
-import { NextPageContext } from 'next'
-import { getSession, signOut } from 'next-auth/react'
+import Navbar from "@/components/Navbar";
+import { NextPageContext } from "next";
+import { getSession } from "next-auth/react";
 
 export async function getServerSideProps(context: NextPageContext) {
-  const session = await getSession(context)
+  const session = await getSession(context);
 
   if (!session) {
     return {
       redirect: {
-        destination: '/auth',
+        destination: "/auth",
         permanent: false,
       },
-    }
+    };
   }
 
   return {
     props: {},
-  }
+  };
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser()
   return (
     <>
-      <h1 className="text-4xl text-green-500">Netflix</h1>
-      <p className="text-white">
-        Logged in as :{user?.name} &{user?.email}
-      </p>
-      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
-        Logout!
-      </button>
+      <Navbar />
     </>
-  )
+  );
 }
--- pages/profiles/index.tsx
+++ pages/profiles/index.tsx
@@ -1,29 +1,63 @@
-import { NextPageContext } from 'next'
-import { getSession } from 'next-auth/react'
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { NextPageContext } from "next";
+import { getSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 export async function getServerSideProps(context: NextPageContext) {
-  const session = await getSession(context)
+  const session = await getSession(context);
 
   if (!session) {
     return {
       redirect: {
-        destination: '/auth',
+        destination: "/auth",
         permanent: false,
       },
-    }
+    };
   }
 
   return {
     props: {},
-  }
+  };
 }
 
 const Profiles = () => {
-  return (
-    <div>
-      <p className="text-white text-4xl">Profiles</p>
-    </div>
-  )
-}
+  const { data: user } = useCurrentUser();
+  const router = useRouter();
 
-export default Profiles
+  return (
+    <div className=" flex items-center h-full justify-center">
+      <div className="flex flex-col">
+        <h1 className="text-3xl md:text-6xl text-white text-center">
+          Who is watching?
+        </h1>
+        <div className="flex items-center justify-center gap-8 mt-10">
+          <div onClick={() => router.push("/")}>
+            <div className="group flex-row w-14 mx-auto">
+              <div
+                className="
+              w-44 
+              h-44 
+              rounded-md 
+              flex 
+              items-center 
+              justify-center 
+              border-2 
+              border-transparent 
+              group-hover:cursor-pointer
+            group-hover:border-white 
+              overflow-hidden"
+              >
+                <img src="/images/default-blue.png" alt="Profiles" />
+              </div>
+              <div className="mt-4 text-gray-400 text-center group-hover:text-white">
+                {user?.name}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profiles;
