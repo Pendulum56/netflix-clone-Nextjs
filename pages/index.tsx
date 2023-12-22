7046a30762bbfed4d8798899deda7e361138005c
tiny <1119663769@qq.com>
2023-12-22 21:51:39 +0800

    首页视频

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,30 @@
-import Navbar from "@/components/Navbar";
-import { NextPageContext } from "next";
-import { getSession } from "next-auth/react";
+import Billboard from '@/components/Billboard'
+import Navbar from '@/components/Navbar'
+import { NextPageContext } from 'next'
+import { getSession } from 'next-auth/react'
 
 export async function getServerSideProps(context: NextPageContext) {
-  const session = await getSession(context);
+  const session = await getSession(context)
 
   if (!session) {
     return {
       redirect: {
-        destination: "/auth",
+        destination: '/auth',
         permanent: false,
       },
-    };
+    }
   }
 
   return {
     props: {},
-  };
+  }
 }
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <Billboard />
     </>
-  );
+  )
 }
